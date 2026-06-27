269f59367a8574b8e7e26b6def4d1f2ef4e34908
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-06-27 20:17:53 -0300

    add yml e data.sql

---
+++ Livro/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_livro (titulo, isbn, editora, ano_publicacao, categoria, quantidade_total, quantidade_disponivel)
+VALUES ('Clean Code', '978-0132350884', 'Pearson', 2008, 'Programação', 5, 5);
+INSERT INTO tb_livro (titulo, isbn, editora, ano_publicacao, categoria, quantidade_total, quantidade_disponivel)
+VALUES ('Domain-Driven Design', '978-0321125217', 'Addison-Wesley', 2003, 'Arquitetura', 3, 3);
+INSERT INTO tb_livro (titulo, isbn, editora, ano_publicacao, categoria, quantidade_total, quantidade_disponivel)
+VALUES ('Chocolate Branco', '978-03245768998', 'Marge clean', 2007, 'Doces', 5, 0);
+INSERT INTO tb_livro (titulo, isbn, editora, ano_publicacao, categoria, quantidade_total, quantidade_disponivel)
+VALUES ('Cereja Verde', '978-9867245367', 'Maria blue', 2013, 'Frutas', 2, 2);
+INSERT INTO tb_livro (titulo, isbn, editora, ano_publicacao, categoria, quantidade_total, quantidade_disponivel)
+VALUES ('xuxu com Tomate', '978-67328950983', 'Jose any', 2015, 'Verduras', 7, 3);
